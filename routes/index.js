a057723c944913a6fbdd7515d3d6f6b4ecdc7c09
Anastasiya Garaga <anastasiya.garaga@spbpu.com>
2020-03-05 16:06:43 +0300

    first commit

---
+++ routes/index.js
@@ -0,0 +1,30 @@
+var express = require('express');
+var router = express.Router();
+const mysqlx = require('@mysql/xdevapi');
+const docs = [];
+
+mysqlx.getSession({
+  host: 'localhost',
+  port: '33060',
+  dbUser: 'root',
+  dbPassword: '4647187Cc'
+  }).then(function (session) {
+  const schema = session.getSchema('world_x');
+  const collection = schema.getCollection('countryinfo');
+  
+  collection
+  .find("GNP*1000000/demographics.Population > 30000")
+  .execute(doc => {
+    docs.push(doc);
+    router.get('/', function(req, res, next) {
+      //res.json(docs);
+      res.render('index', {docs});
+    });
+  })
+  .then((doc) => {
+  })
+  
+  session.close();
+})
+
+module.exports = router;
